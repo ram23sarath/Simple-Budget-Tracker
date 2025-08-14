4a4de6de5358a7794c8d03abf6d83083f14d14e1
ram23sarath <38622885+ram23sarath@users.noreply.github.com>
2025-08-14 12:13:20 +0000

    Added Misc Implementation

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-let transactionDescription = document.getElementById("transaction-desc")
-let transactionAmount = document.getElementById("transaction-amt")
+
+
 let creditSelector = document.getElementById("credit-selector")
 let expenseSelector = document.getElementById("expense-selector")
 let saveTransaction = document.getElementById("save-btn")
+//const totalAmountFromLocalStorage = JSON.parse(localStorage.getItem("****"))
+
 
 let allTransactions = []
 
+saveTransaction.addEventListener("submit", function () {
+    event.preventDefault(); 
+    const transactionDescription = document.getElementById("transaction-desc").value
+    const transactionAmount = document.getElementById("transaction-amt").value
+})
+
+function renderdata(args) {
+    
+}
