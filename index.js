2aa9b7e6f69bb0a9c021d784c770130eb4dcbcae
ram23sarath <38622885+ram23sarath@users.noreply.github.com>
2025-08-14 10:55:35 +0000

    Added Misc Implementation

---
+++ index.js
@@ -0,0 +1,6 @@
+let transactionDescription = document.getElementById("transaction-desc")
+let transactionAmount = document.getElementById("transaction-amt")
+let creditSelector = document.getElementById("credit-selector")
+let expenseSelector = document.getElementById("expense-selector")
+let saveTransaction = document.getElementById("save-btn")
+
